fbf6d8a686a4ba6b87c368b4c7e38dcd943a0e85
rafa <rafa@envy.(none)>
2010-11-10 14:25:23 +0100

    Issue 2 - "no such file to load -- activerecord"

--- lib/foreign_key_migrations.rb
+++ lib/foreign_key_migrations.rb
@@ -1,3 +1,5 @@
+require 'active_record'
+
 ActiveRecord::Base.send(:include, RedHillConsulting::ForeignKeyMigrations::ActiveRecord::Base)
 ActiveRecord::Migration.send(:include, RedHillConsulting::ForeignKeyMigrations::ActiveRecord::Migration)
 ActiveRecord::ConnectionAdapters::TableDefinition.send(:include, RedHillConsulting::ForeignKeyMigrations::ActiveRecord::ConnectionAdapters::TableDefinition)
